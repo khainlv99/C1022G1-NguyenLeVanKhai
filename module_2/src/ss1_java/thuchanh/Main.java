b7d43d128b3160046fdf404bf75e8343fbb81046
khainlv99 <nguyenlevankhai@gmail.com>
2022-12-02 11:43:06 +0700

    update

---
+++ module_2/src/ss1_java/thuchanh/Main.java
@@ -0,0 +1,18 @@
+package ss1_java.thuchanh;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 5.2f;
+        double c = 20.5;
+        boolean d = true;
+        char e = 'a';
+        String r = "Khải";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(r);
+    }
+}
