9c686cefcc8549d4afbcd2f289d04f201a22a284
khainlv99 <nguyenlevankhai@gmail.com>
2022-12-04 16:29:19 +0700

    update

---
+++ module_2/src/ss2_vonglap/baitap/SoNguyenTo.java
@@ -0,0 +1,22 @@
+package ss2_vonglap.baitap;
+
+public class SoNguyenTo {
+    public static void main(String[] args) {
+        int number = 20;
+        int count = 0;
+        int n = 2;
+        while (count < number) {
+            boolean check = true;
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    check = false;
+                }
+            }
+            if (check) {
+                System.out.print(n + ", ");
+                count++;
+            }
+            n++;
+        }
+    }
+}
