b12d032897aff2370ae918c9f6ad6575b1f84aeb
khainlv99 <nguyenlevankhai@gmail.com>
2022-12-04 19:19:48 +0700

    update

---
+++ module_2/src/ss3_mang_phuongthuc/baitap/DemChuoi.java
@@ -0,0 +1,19 @@
+package ss3_mang_phuongthuc.baitap;
+
+import java.util.Scanner;
+
+public class DemChuoi {
+    public static void main(String[] args) {
+        String str = "khainguyenlevan";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập ký tự: ");
+        char c = scanner.nextLine().charAt(0);
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == c) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
