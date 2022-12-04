59db8b2286a762d1d6be05962942708d448dd73c
khainlv99 <nguyenlevankhai@gmail.com>
2022-12-04 19:33:56 +0700

    update

---
+++ module_2/src/ss4_lop_doituong/baitap/phuongtrinhbac2/QuadraticEquation.java
@@ -0,0 +1,28 @@
+package ss4_lop_doituong.baitap.phuongtrinhbac2;
+
+public class QuadraticEquation {
+    private double a,b,c;
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public double getA() {
+        return this.a;
+    }
+    public double getB() {
+        return this.b;
+    }
+    public double getC() {
+        return this.c;
+    }
+    public double getDiscriminant() {
+        return this.b*2 - 4*this.a*this.c;
+    }
+    public double getRoot1() {
+        return (-this.b + Math.pow((this.b*this.b- 4*this.a*this.c),0.5))/(2*this.a);
+    }
+    public   double getRoot2() {
+        return (-this.b - Math.pow((this.b*this.b- 4*this.a*this.c),0.5))/(2*this.a);
+    }
+}
---
+++ module_2/src/ss4_lop_doituong/baitap/phuongtrinhbac2/RunQuadraticEquation.java
@@ -0,0 +1,26 @@
+package ss4_lop_doituong.baitap.phuongtrinhbac2;
+import java.util.Scanner;
+
+public class RunQuadraticEquation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập a: ");
+        double a = scanner.nextDouble();
+        System.out.println("Nhập b: ");
+        double b = scanner.nextDouble();
+        System.out.println("nhập c: ");
+        double c = scanner.nextDouble();
+        QuadraticEquation quadraticEquation = new QuadraticEquation(a, b, c);
+        double delta = quadraticEquation.getDiscriminant();
+        System.out.println("Delta: " + delta);
+        if (delta > 0) {
+            System.out.println("Phương trình có 2 nghiệm: ");
+            System.out.println("Nghiệm 1 :" + quadraticEquation.getRoot1());
+            System.out.println("Nghiệm 2 : " + quadraticEquation.getRoot2());
+        } else if (delta < 0) {
+            System.out.println("Phương trình vô nghiệm");
+        } else {
+            System.out.println("Phương trình có nghiệm kép: " + quadraticEquation.getRoot2());
+        }
+    }
+}
