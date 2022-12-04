c3fe84c930b385b3eefee0831fa2d9aa5a48990d
khainlv99 <nguyenlevankhai@gmail.com>
2022-12-04 16:50:36 +0700

    update

--- module_2/src/ss3_mang_phuongthuc/baitap/GopMang.java
+++ module_2/src/ss3_mang_phuongthuc/baitap/GopMang.java
@@ -17,15 +17,15 @@ public class GopMang {
             System.out.println("Nhập giá trị của mảng 2: ");
             arr2[i] = scanner.nextInt();
         }
-        int[] arrNum3 = new int[arr1.length + arr2.length];
+        int[] arr3 = new int[arr1.length + arr2.length];
         for (int i = 0; i < arr1.length; i++) {
-            arrNum3[i] = arr1[i];
+            arr3[i] = arr1[i];
         }
         for (int i = 0; i < arr2.length; i++) {
-            arrNum3[(arr1.length) + i] = arr2[i];
+            arr3[(arr1.length) + i] = arr2[i];
         }
-        for (int i = 0; i < arrNum3.length; i++) {
-            System.out.print(arrNum3[i] + ", ");
+        for (int i = 0; i < arr3.length; i++) {
+            System.out.print(arr3[i] + ", ");
         }
     }
 }
