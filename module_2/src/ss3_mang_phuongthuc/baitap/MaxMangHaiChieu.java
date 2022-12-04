d250ef8d86f97fc6f3f048f73f72c5bfea93d269
khainlv99 <nguyenlevankhai@gmail.com>
2022-12-04 16:57:40 +0700

    update

---
+++ module_2/src/ss3_mang_phuongthuc/baitap/MaxMangHaiChieu.java
@@ -0,0 +1,37 @@
+package ss3_mang_phuongthuc.baitap;
+
+import java.util.Scanner;
+
+public class MaxMangHaiChieu {
+    public static void main(String[] args) {
+        int row;
+        int col;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter size of row: ");
+        row = Integer.parseInt(scanner.nextLine());
+        System.out.println("Enter size of col");
+        col = Integer.parseInt(scanner.nextLine());
+        int[][] arr = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.println("Enter value for element of row "+ (i+1) + " col " + (j+1) +" : ");
+                arr[i][j] = Integer.parseInt(scanner.nextLine());
+            }
+        }
+        int max = arr[0][0];
+        int indexRow = 0;
+        int indexCol = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if(arr[i][j] > max ) {
+                    max = arr[i][j];
+                    indexRow=i;
+                    indexCol=j;
+                }
+            }
+        }
+        System.out.println("Số lớn nhất là: "+ max );
+        System.out.println("Hàng chỉ mục:  "+indexRow );
+        System.out.println("Cột chỉ mục:  "+indexCol );
+    }
+}
