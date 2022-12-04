d3af9f9d172aa5c7cbe5ea118b9093f9794219c9
khainlv99 <nguyenlevankhai@gmail.com>
2022-12-04 19:42:36 +0700

    update

---
+++ module_2/src/ss4_lop_doituong/baitap/fan/Fan.java
@@ -0,0 +1,61 @@
+package ss4_lop_doituong.baitap.fan;
+
+public class Fan {
+    public static final int SLOW = 1;
+    public static final int MEDIUM= 2;
+    public static final int FAST = 3;
+    private int speed = SLOW;
+    private boolean on = false;
+    private double radius = 5;
+    private String color = "blue";
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    @Override
+    public String toString() {
+        if(this.on) {
+            return "Fan{" +
+                    "speed=" + speed +
+                    ", fan is on" +
+                    ", radius=" + radius +
+                    ", color='" + color + '\'' +
+                    '}';
+        } else {
+            return "Fan{" +
+                    "speed = 0"   +
+                    ", fan is off" +
+                    ", radius=" + radius +
+                    ", color='" + color + '\'' +
+                    '}';
+        }
+    }
+}
