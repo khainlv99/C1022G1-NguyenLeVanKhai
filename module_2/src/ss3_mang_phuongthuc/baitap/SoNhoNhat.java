db8c4993f7db94861e0a6b5fb94ff39970572a8b
khainlv99 <nguyenlevankhai@gmail.com>
2022-12-04 16:54:01 +0700

    update

---
+++ module_2/src/ss3_mang_phuongthuc/baitap/SoNhoNhat.java
@@ -0,0 +1,22 @@
+package ss3_mang_phuongthuc.baitap;
+
+import java.util.Scanner;
+
+public class SoNhoNhat {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+
+        for (int i = 0; i < arr.length; i++) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Nhập phần tử có trong mảng  " + (i + 1) + ": ");
+            arr[i] = scanner.nextInt();
+        }
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Số nhỏ nhất là : " + min);
+    }
+}
