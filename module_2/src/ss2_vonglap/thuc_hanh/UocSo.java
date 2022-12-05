526cf9e8e2e7b5c7b87d6f0d8348d5f976fa3b0a
khainlv99 <117066707+khainlv99@users.noreply.github.com>
2022-12-05 14:56:27 +0700

    update

---
+++ module_2/src/ss2_vonglap/thuc_hanh/UocSo.java
@@ -0,0 +1,28 @@
+package ss2_vonglap.thuchanh;
+
+import java.util.Scanner;
+
+public class UocSo {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Mời bạn nhập số a: ");
+        a = scanner.nextInt();
+        System.out.println("Mời bạn nhập số b: ");
+        b = scanner.nextInt();
+        a=Math.abs(a);
+        b=Math.abs(b);
+        if(a==0 || b==0){
+            System.out.println("No greatest common factor");
+        }
+        while (a!=b){
+            if (a>b){
+                a = a - b;
+            }else {
+                b = b - a;
+            }
+        }
+        System.out.println("Greatest common factor: " + a);
+    }
+}
---
+++ module_2/src/ss3_mang_phuongthuc/thuc_hanh/DaoNguoc.java
@@ -0,0 +1,12 @@
+package ss3_mang_phuongthuc.thuchanh;
+
+import java.util.Scanner;
+
+public class DaoNguoc {
+    public static void main(String[] args) {
+        int size;
+        int arr[];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Mời ");
+    }
+}
