764e3a87d83917765e28c9dd1f1f8c9342a68e04
khainlv99 <117066707+khainlv99@users.noreply.github.com>
2022-12-26 11:04:11 +0700

    update

--- module_2/src/ss19_String_Regex/ValidateName.java
+++ module_2/src/ss19_String_Regex/ValidateName.java
@@ -1,24 +1,21 @@
-package ss19_String_Regex1;
+package ss19_String_Regex;
 
 import java.util.Scanner;
 
-public class Validate {
+public class ValidateName {
     public static final String REGEX_NAME = "^[CAP]+\\d{4}+[G-M]$";
 
     public static boolean checkClass(String nameClass) {
         return nameClass.matches(REGEX_NAME);
     }
 
-
     public static void main(String[] args) {
         System.out.print("Enter your class name : ");
         Scanner scanner = new Scanner(System.in);
         String className = scanner.nextLine();
         if (checkClass(className)) {
-            System.out.println(className);
             System.out.println("True");
-        } else {
-            System.out.println(className);
+        }else{
             System.out.println("False");
         }
     }
