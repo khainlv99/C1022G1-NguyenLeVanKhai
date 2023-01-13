3ad194050af5f9a9e9944dd7970958fa41a6dc1c
khainlv99 <117066707+khainlv99@users.noreply.github.com>
2023-01-13 15:25:00 +0700

    update

--- module_3/Casestudy/furama_handle.sql
+++ module_3/Casestudy/furama_handle.sql
@@ -4,13 +4,18 @@ use furama;
 select * from nhan_vien where ho_ten like 'H%' or ho_ten like 'T%' or ho_ten like 'K%' and LENGTH(ho_ten) <= 15;
 
 -- câu 3
--- SELECT dia_chi, ngay_sinh, TIMESTAMPDIFF(YEAR, ngay_sinh, CURRENT_DATE) as age
--- from khach_hang
--- having age between 18 and 50
--- and (dia_chi = 'Đà Nẵng' or dia_chi = 'Quảng Trị');
--- câu 4
-select ma_khach_hang, count(ma_khach_hang) as 'so_lan_dat_phong'
+select *
 from khach_hang
-where ma_loai_khach = 1
-group by ma_khach_hang 
-order by count(ma_khach_hang) asc;
+where (round(datediff(curdate(),khach_hang.ngay_sinh)/365,0)>=18
+and round(datediff(curdate(),khach_hang.ngay_sinh)/365,0)<=50)
+and ((khach_hang.dia_chi like '%Đà Nẵng%' )
+or (khach_hang.dia_chi like '%Quảng Trị%'));
+-- câu 4
+select kh.ma_khach_hang	,
+kh.ho_ten,
+count(hd.ma_khach_hang) 'total'
+from khach_hang kh join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+join hop_dong hd on hd.ma_khach_hang = kh.ma_khach_hang
+where lk.ma_loai_khach=1
+group by kh.ma_khach_hang
+order by count(hd.ma_khach_hang);
