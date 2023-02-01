af902c063df4c6355660d3ae5e578560f631f6d2
khainlv99 <117066707+khainlv99@users.noreply.github.com>
2023-02-01 20:49:09 +0700

    update

--- module_3/Casestudy/furama_handle.sql
+++ module_3/Casestudy/furama_handle.sql
@@ -97,41 +97,35 @@ FROM
     khach_hang;
     -- task 6:Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ 
 -- chưa từng được khách hàng thực hiện đặt quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
-select
-	dich_vu.ma_dich_vu, 
-    dich_vu.ten_dich_vu, 
-    dich_vu.dien_tich, 
-    dich_vu.chi_phi_thue, 
-    loai_dich_vu.ten_loai_dich_vu
     
--- select 
---   dich_vu.ma_dich_vu, 
---   dich_vu.ten_dich_vu, 
---   dich_vu.dien_tich, 
---   dich_vu.chi_phi_thue, 
---   loai_dich_vu.ten_loai_dich_vu, 
---   hop_dong.ngay_lam_hop_dong 
--- from 
---   dich_vu 
---   join loai_dich_vu on loai_dich_vu.ma_loai_dich_vu = dich_vu.ma_loai_dich_vu 
---   join hop_dong on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu 
--- where 
---   dich_vu.ma_dich_vu not in (
---     select 
---       hop_dong.ma_dich_vu 
---     from 
---       hop_dong 
---     where 
---       (
---         (
---           month(hop_dong.ngay_lam_hop_dong) between 1 
---           and 3
---         ) 
---         and year(hop_dong.ngay_lam_hop_dong)= 2021
---       )
---   ) 
--- group by 
---   dich_vu.ten_dich_vu;
+select 
+  dich_vu.ma_dich_vu, 
+  dich_vu.ten_dich_vu, 
+  dich_vu.dien_tich, 
+  dich_vu.chi_phi_thue, 
+  loai_dich_vu.ten_loai_dich_vu, 
+  hop_dong.ngay_lam_hop_dong 
+from 
+  dich_vu 
+  join loai_dich_vu on loai_dich_vu.ma_loai_dich_vu = dich_vu.ma_loai_dich_vu 
+  join hop_dong on hop_dong.ma_dich_vu = dich_vu.ma_dich_vu 
+where 
+  dich_vu.ma_dich_vu not in (
+    select 
+      hop_dong.ma_dich_vu 
+    from 
+      hop_dong 
+    where 
+      (
+        (
+          month(hop_dong.ngay_lam_hop_dong) between 1 
+          and 3
+        ) 
+        and year(hop_dong.ngay_lam_hop_dong)= 2021
+      )
+  ) 
+group by 
+  dich_vu.ten_dich_vu;
 -- task 7: Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, 
 -- ten_loai_dich_vu của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
 SHOW VARIABLES LIKE 'sql_mode';
@@ -301,7 +295,7 @@ having
     ifnull(hdct.so_luong, 0)
   ) >= (
     select 
-      max(hop_dong_chi_tiet.so_luong) 
+      max(hop_dong_chi_tiet.so_luong)
     from 
       hop_dong_chi_tiet
   );
